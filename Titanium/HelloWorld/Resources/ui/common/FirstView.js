0a39aa86c54b9e467332197bb123ab390ba975e4
Peter Friese <peter@peterfriese.de>
2012-09-12 15:38:38 +0200

    Enhanced Hello World app.

--- Titanium/HelloWorld/Resources/ui/common/FirstView.js
+++ Titanium/HelloWorld/Resources/ui/common/FirstView.js
@@ -1,7 +1,29 @@
 //FirstView Component Constructor
 function FirstView() {
 	//create object instance, a parasitic subclass of Observable
-	var self = Ti.UI.createView();
+	var self = Ti.UI.createView({
+		layout: 'vertical'
+	});
+	
+	var textField = Ti.UI.createTextField({
+	    borderStyle : Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	    hintText : 'Enter your name, please',
+	    top : 10,
+	    width : 'auto', 
+	    height : 'auto'
+	});
+	self.add(textField);
+	
+	var button = Ti.UI.createButton({
+		title:'Greet',
+		top: 20,
+	    width : 'auto', 
+	    height : 'auto'		
+	});
+	self.add(button);
+	button.addEventListener('click', function(e) {
+		label.text = 'Hello, ' + textField.value
+	});
 	
 	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
 	var label = Ti.UI.createLabel({
